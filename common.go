8930cfe0e541cbf712294a367e352806e3b9fc83
Jeff Wendling <jeff@spacemonkey.com>
2018-01-03 18:54:06 -0700

    simplify, add concurrency limit, remove checksum

--- common.go
+++ common.go
@@ -2,25 +2,16 @@
 package admission
 
 import (
-	"encoding/binary"
-	"hash/crc32"
+	"context"
 	"net"
 	"sync"
-
-	"gopkg.in/spacemonkeygo/monkit.v2"
-)
-
-var (
-	mon = monkit.Package()
-
-	messagesRead = mon.IntVal("messages_read")
 )
 
 // Handler is a type that can handle messages.
 type Handler interface {
 	// Handle should do things with the message Data. No fields of the message
 	// should be held on to after the call has returned.
-	Handle(m *Message)
+	Handle(ctx context.Context, m *Message)
 }
 
 type Message struct {
@@ -40,18 +31,6 @@ type Message struct {
 	// points at buf. array to avoid an allocation because we need a [][]byte
 	// pointed at buf eventually.
 	buffers [1][]byte
-}
-
-// castTable is used for our checksums
-var castTable = crc32.MakeTable(crc32.Castagnoli)
-
-// AddChecksum appends the required checksum to the buf for admission to accept
-// the message.
-func AddChecksum(buf []byte) []byte {
-	var scratch [4]byte
-	check := crc32.Checksum(buf, castTable)
-	binary.BigEndian.PutUint32(scratch[:], check)
-	return append(buf, scratch[:]...)
 }
 
 func newMessage() (m *Message) {
--- dispatch.go
+++ dispatch.go
@@ -2,8 +2,6 @@ package admission
 
 import (
 	"context"
-	"encoding/binary"
-	"hash/crc32"
 	"log"
 	"net"
 	"runtime"
@@ -11,9 +9,15 @@ import (
 	"golang.org/x/net/ipv4"
 )
 
-// DefaultMessages is the number of Messages passed to a ReadBatch call in the
-// Dispatcher Run loop.
-const DefaultMessages = 128
+const (
+	// DefaultMessages is the number of Messages passed to a ReadBatch call
+	// in the Dispatcher Run loop.
+	DefaultMessages = 128
+
+	// DefaultInFlight is the number of concurrent calls to the Handler
+	// allowed in the Run loop. If it would go over, the message is dropped.
+	DefaultInFlight = 256
+)
 
 // Dispatcher reads Messages on the PacketConn and forwards them into the
 // Handler in parallel.
@@ -30,6 +34,10 @@ type Dispatcher struct {
 
 	// Flags are optinally flags to be passed to the batch read method.
 	Flags int
+
+	// InFlight controls the number of parallel calls to the Handler. Zero is
+	// DefaultInFlight.
+	InFlight int
 }
 
 func (d *Dispatcher) Run(ctx context.Context) (err error) {
@@ -37,10 +45,15 @@ func (d *Dispatcher) Run(ctx context.Context) (err error) {
 	if num_messages == 0 {
 		num_messages = DefaultMessages
 	}
+	in_flight := d.InFlight
+	if in_flight == 0 {
+		in_flight = DefaultInFlight
+	}
 
 	pc := ipv4.NewPacketConn(d.PacketConn)
 	in := make([]*Message, num_messages)
 	msgs := make([]ipv4.Message, num_messages)
+	sem := make(chan struct{}, in_flight)
 
 	for {
 		// fill in any nil messages, and build up the ipv4.Message array for
@@ -58,47 +71,39 @@ func (d *Dispatcher) Run(ctx context.Context) (err error) {
 		if err != nil {
 			return err
 		}
-		messagesRead.Observe(int64(n))
 
 		// fix up the Data slices, pass them off to be handled in a goroutine
 		// and clear them out of the in array for the next round of packets.
 		for i := 0; i < n; i++ {
 			in[i].Data = in[i].buf[:msgs[i].N]
 			in[i].RemoteAddr = msgs[i].Addr
-			go handleMessage(ctx, d.Handler, in[i])
-			in[i] = nil
+
+			select {
+			case sem <- struct{}{}:
+				go handleMessage(ctx, sem, d.Handler, in[i])
+				in[i] = nil
+			default:
+			}
 		}
 	}
 }
 
 // handleMessage passes the message to the handler and returns it to the pool
 // once it is done. it is the layer of safety around panics.
-func handleMessage(ctx context.Context, h Handler, m *Message) {
-	// first check that the message's crc is valid. silently drop any packets
-	// that don't checksum.
-	offset := len(m.Data) - 4
-	if offset < 0 {
-		return
-	}
-	check := crc32.Checksum(m.Data[:offset], castTable)
-	got := binary.BigEndian.Uint32(m.Data[offset:])
-	if check != got {
-		return
-	}
+func handleMessage(ctx context.Context, sem chan struct{}, h Handler,
+	m *Message) {
 
 	// we pass addr in case m.RemoteAddr is changed during the update call
 	defer func(m *Message, addr net.Addr) {
 		if v := recover(); v != nil {
-			// get the stack and ensure it ends with a newline
 			buf := make([]byte, 64<<10)
 			buf = buf[:runtime.Stack(buf, false)]
 			log.Printf("admission: panic from %v: %v\n%s", addr, v, buf)
 		}
 
 		putMessage(m)
+		<-sem
 	}(m, m.RemoteAddr)
 
-	// slice it off and pass it on
-	m.Data = m.Data[:offset]
-	h.Handle(m)
+	h.Handle(ctx, m)
 }
